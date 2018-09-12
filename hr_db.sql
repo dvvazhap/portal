e04c7671219a5f9361dbaf4d13302c6013698f75
dvvazhap <dh!vyal@12345>
2018-09-12 10:25:54 +0530

    adding db

--- hr_db.sql
+++ hr_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 07, 2018 at 05:09 AM
+-- Generation Time: Sep 12, 2018 at 04:55 AM
 -- Server version: 5.7.19
--- PHP Version: 7.0.23
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `hr_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+DROP TABLE IF EXISTS `education`;
+CREATE TABLE IF NOT EXISTS `education` (
+  `email` varchar(50) NOT NULL,
+  `ind` int(3) NOT NULL,
+  `college` varchar(50) DEFAULT NULL,
+  `degree` varchar(50) DEFAULT NULL,
+  `stream` varchar(50) DEFAULT NULL,
+  `start` varchar(7) DEFAULT NULL,
+  `end` varchar(7) DEFAULT NULL,
+  `cgpa` float DEFAULT '0',
+  `percentage` float DEFAULT '0',
+  UNIQUE KEY `email` (`email`,`ind`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -44,9 +64,7 @@ CREATE TABLE IF NOT EXISTS `employerinfo` (
 --
 
 INSERT INTO `employerinfo` (`email`, `name`, `designation`, `org_name`, `city`, `phone`) VALUES
-('dijil2208@gmail.com', 'Dijil', 'SSE', 'Samsung', 'Bangalore', '9739091906'),
-('dijil2208@gmail.comv', NULL, NULL, NULL, NULL, NULL),
-('info.werthere4u@gmail.com', NULL, NULL, NULL, NULL, NULL);
+('info.werthere4u@gmail.com', 'Owner', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -66,15 +84,8 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 --
 
 INSERT INTO `feedback` (`email`, `subject`, `profile`) VALUES
-('undefined', 'dijil2208@gmail.com', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'undefined', 1),
-('dijil2208@gmail.com', 'dsfg', 1);
+('dijil2208@gmail.com', 'heloooo', 1),
+('dijil2208@gmail.com', 'dsxfsdf', 1);
 
 -- --------------------------------------------------------
 
@@ -85,7 +96,7 @@ INSERT INTO `feedback` (`email`, `subject`, `profile`) VALUES
 DROP TABLE IF EXISTS `openings`;
 CREATE TABLE IF NOT EXISTS `openings` (
   `ind` int(3) NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `timestamp` varchar(20) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `fullTime` int(1) NOT NULL DEFAULT '1',
   `partTime` int(1) NOT NULL DEFAULT '0',
@@ -99,19 +110,36 @@ CREATE TABLE IF NOT EXISTS `openings` (
   `skills` varchar(300) DEFAULT NULL,
   `specificReq` varchar(300) DEFAULT NULL,
   `noticePeriod` int(3) DEFAULT NULL,
+  `gender` varchar(11) NOT NULL DEFAULT 'Anyone',
+  `count` int(3) NOT NULL DEFAULT '1',
+  `viewers` int(5) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ind`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `openings`
+-- Table structure for table `projects`
 --
 
-INSERT INTO `openings` (`ind`, `timestamp`, `email`, `fullTime`, `partTime`, `intern`, `designation`, `company`, `location`, `contact`, `min_years`, `max_years`, `skills`, `specificReq`, `noticePeriod`) VALUES
-(28, '2018-06-05 18:34:19', 'dijil2208@gmail.com', 1, 0, 0, 'sse', 'Samsung', 'Chennai', '78789798', 0, 1, 'Angular', '15 lacs', 10),
-(31, '2018-06-05 18:37:26', 'dijil2208@gmail.com', 1, 0, 0, 'dfsd', 'fsdfsdf', 'sdfsdf', '34534534', 0, 0, 'dsfsdf', 'sdfsdf', 30),
-(32, '2018-06-05 18:38:36', 'dijil2208@gmail.com', 1, 0, 0, 'zxczx', 'czxc', 'zxcz', '43534534', 0, 0, 'dsfsdf', 'dsfsdf', 320),
-(30, '2018-06-05 18:36:07', 'dijil2208@gmail.com', 1, 1, 1, 'bbbbb', 'bbbb', 'bbbb', '456456546', 0, 10, 'sddasd', 'asdas', 112),
-(29, '2018-06-05 18:35:46', 'dijil2208@gmail.com', 1, 1, 0, 'aaaa', 'aaaa', 'aaaa', '677868768', 0, 56, 'ghgjhgj', 'hgjhghj', 10);
+DROP TABLE IF EXISTS `projects`;
+CREATE TABLE IF NOT EXISTS `projects` (
+  `email` varchar(50) NOT NULL,
+  `ind` int(3) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `skills` varchar(500) DEFAULT NULL,
+  `start` varchar(7) DEFAULT NULL,
+  `end` varchar(7) DEFAULT NULL,
+  UNIQUE KEY `email` (`email`,`ind`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`email`, `ind`, `name`, `description`, `skills`, `start`, `end`) VALUES
+('dijil2208@gmail.com', 1, 'dfgdfg', '', '', '2015-12', '2016-03');
 
 -- --------------------------------------------------------
 
@@ -129,17 +157,21 @@ CREATE TABLE IF NOT EXISTS `skills` (
   `partTime` int(1) DEFAULT '0',
   `intern` int(1) DEFAULT '0',
   `designation` varchar(30) DEFAULT NULL,
-  `company` varchar(30) DEFAULT NULL,
-  `cur_location` varchar(30) DEFAULT NULL,
   `fut_location` varchar(100) DEFAULT NULL,
   `experience` float DEFAULT NULL,
   `noticePeriod` int(3) DEFAULT NULL,
-  `degree` varchar(30) DEFAULT NULL,
-  `stream` varchar(30) DEFAULT NULL,
-  `institution` varchar(30) DEFAULT NULL,
-  `passout` int(4) DEFAULT NULL,
-  `skills` varchar(300) DEFAULT NULL,
-  `specificReq` varchar(300) DEFAULT NULL,
+  `skills` varchar(1000) DEFAULT NULL,
+  `specificReq` varchar(1000) DEFAULT NULL,
+  `gender` varchar(11) NOT NULL DEFAULT 'Anyone',
+  `viewers` int(5) NOT NULL DEFAULT '1',
+  `objective` varchar(1000) DEFAULT NULL,
+  `languages` varchar(200) DEFAULT NULL,
+  `academic_ach` varchar(1000) DEFAULT NULL,
+  `extra_curricular` varchar(1000) DEFAULT NULL,
+  `certifications` varchar(1000) DEFAULT NULL,
+  `hobbies` varchar(1000) DEFAULT NULL,
+  `address` varchar(1000) DEFAULT NULL,
+  `linkedin` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -147,8 +179,8 @@ CREATE TABLE IF NOT EXISTS `skills` (
 -- Dumping data for table `skills`
 --
 
-INSERT INTO `skills` (`email`, `name`, `phone`, `looking`, `fullTime`, `partTime`, `intern`, `designation`, `company`, `cur_location`, `fut_location`, `experience`, `noticePeriod`, `degree`, `stream`, `institution`, `passout`, `skills`, `specificReq`) VALUES
-('dijil2208@gmail.coma', 'Dijil234234', '324356756', 1, 1, 1, 1, 'ashjdj', 'hkjh', 'kjh', 'kjh', 3.4435, 70, 'sajknbjkn', 'njk', 'nkjn', 2018, 'asdasd', 'asdasd');
+INSERT INTO `skills` (`email`, `name`, `phone`, `looking`, `fullTime`, `partTime`, `intern`, `designation`, `fut_location`, `experience`, `noticePeriod`, `skills`, `specificReq`, `gender`, `viewers`, `objective`, `languages`, `academic_ach`, `extra_curricular`, `certifications`, `hobbies`, `address`, `linkedin`) VALUES
+('dijil2208@gmail.com', 'DIJIL VV', '', 1, 1, 0, 0, 'Lead Engineer', 'Bangalore', 4.8, 30, 'Angular', '', 'Male', 1, '', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -165,6 +197,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `validity` varchar(30) NOT NULL DEFAULT 'lifetime',
   `user_type` int(2) NOT NULL,
   `reset_code` int(11) DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -172,9 +205,26 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`token`, `password`, `email`, `v_profile`, `validity`, `user_type`, `reset_code`) VALUES
-('1528311708932info.werthere4u@gmail.com', '6a64e78b762e44c0d25b81c152dcdbfe', 'info.werthere4u@gmail.com', 1, 'lifetime', 3, NULL),
-('1527966559098dijil2208@gmail.com', '25d55ad283aa400af464c76d713c07ad', 'dijil2208@gmail.com', 1, 'lifetime', 1, NULL);
+INSERT INTO `users` (`token`, `password`, `email`, `v_profile`, `validity`, `user_type`, `reset_code`, `name`) VALUES
+('1536727493607dijil2208@gmail.com', '2f9453c5ab5205dace6ad5b1618aba1c', 'dijil2208@gmail.com', 1, 'lifetime', 2, NULL, 'dijil'),
+('1536727672641info.werthere4u@gmail.com', '2f9453c5ab5205dace6ad5b1618aba1c', 'info.werthere4u@gmail.com', 1, 'lifetime', 3, NULL, 'Owner');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_experience`
+--
+
+DROP TABLE IF EXISTS `work_experience`;
+CREATE TABLE IF NOT EXISTS `work_experience` (
+  `email` varchar(50) NOT NULL,
+  `ind` int(3) NOT NULL,
+  `company` varchar(50) DEFAULT NULL,
+  `start` varchar(7) DEFAULT NULL,
+  `end` varchar(7) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  UNIQUE KEY `email` (`email`,`ind`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
