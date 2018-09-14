74121f5266f61f9c07c7f1aa2b1d1f3df4c1f30f
dvvazhap <dh!vyal@12345>
2018-09-14 10:28:35 +0530

    handling escape sequences

--- authentication.js
+++ authentication.js
@@ -3,7 +3,7 @@ let logger = require('./log/logModule');
 var con = require('./db');
 
 exports.validateUser = function (email, req, res) {
-    let sql = "select token, v_profile from users where email = '" + email + "'";
+    let sql = "select token, v_profile from users where email = '" + email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in validateEmail", error.sqlMessage);
@@ -23,7 +23,7 @@ exports.validateUser = function (email, req, res) {
 }
 
 exports.verifyUser = function (body, res) {
-    let sql = "SELECT v_profile from users where token = '" + body.token + "' AND email = '" + body.email + "'";
+    let sql = "SELECT v_profile from users where token = '" + body.token.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' AND email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in verifyUser", error.sqlMessage);
@@ -31,7 +31,7 @@ exports.verifyUser = function (body, res) {
         } else if (result) {
             if (result && result[0] && result[0].v_profile && result[0].v_profile == 1) res.status(200).send("verified");
             else {
-                let sql1 = "UPDATE users SET v_profile=1 where token = '" + body.token + "' AND v_profile = '" + body.code + "' AND email = '" + body.email + "'";
+                let sql1 = "UPDATE users SET v_profile=1 where token = '" + body.token.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' AND v_profile = '" + body.code + "' AND email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
                 con.query(sql1, (error1, result1, field1) => {
                     if (error1) {
                         logger.log("error", "Error in verifyUser", error1.sqlMessage);
@@ -47,13 +47,13 @@ exports.verifyUser = function (body, res) {
 
 exports.resetPasswordLink = function (body, res) {
     let r = Math.floor(Math.random(1, 1000000000) * 5345354346) % 1000000000;
-    let sql1 = "UPDATE users SET reset_code = " + r + " where email = '" + body.email + "'";
+    let sql1 = "UPDATE users SET reset_code = " + r + " where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql1, (error1, result1, field1) => {
         if (error1) {
             logger.log("error", "Error in resetPasswordLink", error1.sqlMessage);
             res.status(500).send(error1.sqlMessage);
         } else if (result1) {
-            let sql2 = "SELECT name FROM users WHERE email = '" + body.email + "'";
+            let sql2 = "SELECT name FROM users WHERE email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
             let name = body.email;
             con.query(sql2, (error2, result2, field2) => {
                 if (error2) {
@@ -72,7 +72,7 @@ exports.resetPasswordLink = function (body, res) {
 }
 
 exports.resetPassword = function (body, res) {
-    let sql = "SELECT reset_code from users where email = '" + body.email + "'";
+    let sql = "SELECT reset_code from users where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in resetPassword", error.sqlMessage);
@@ -80,7 +80,7 @@ exports.resetPassword = function (body, res) {
         } else if (result) {
             let r = Math.floor(Math.random(1, 1000000000) * 5345354346) % 1000000000;
             if (result && result[0] && result[0].reset_code && result[0].reset_code == body.code) {
-                let sql1 = "UPDATE users SET password='" + body.password + "',reset_code='" + r + "' where email = '" + body.email + "'";
+                let sql1 = "UPDATE users SET password='" + body.password + "',reset_code='" + r + "' where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
                 con.query(sql1, (error1, result1, field1) => {
                     if (error1) {
                         logger.log("error", "Error in resetPassword", error1.sqlMessage);
@@ -98,7 +98,7 @@ exports.resetPassword = function (body, res) {
 }
 
 exports.checkUser = function (body, res) {
-    let sql = "SELECT token,user_type,v_profile, COUNT(`email`) AS count from `users` where email = '" + body.email + "' AND password='" + body.password + "'";
+    let sql = "SELECT token,user_type,v_profile, COUNT(`email`) AS count from `users` where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' AND password='" + body.password + "'";
 
     con.query(sql, (error, result, field) => {
         if (error) {
@@ -113,7 +113,8 @@ exports.checkUser = function (body, res) {
 
 exports.addUser = function (body, res) {
     let r = Math.floor(Math.random(1, 1000000000) * 5345354346) % 1000000000;
-    let data = { token: body.token, password: body.password, email: body.email, user_type: body.user_type, v_profile: r, name: body.name };
+    let data = { token: body.token.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), password: body.password, email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), 
+    user_type: body.user_type, v_profile: r, name: body.name.replace(/\\/g,'\\\\').replace(/'/g, "\\'") };
     // console.log("data",data);
     con.query("INSERT INTO `users` SET ? ", data, (error, result, field) => {
         if (error) {
@@ -123,7 +124,7 @@ exports.addUser = function (body, res) {
         else if (result) {
             email.sendVerificationMail(body.name, body.email, body.token, r, body.user_type);
             if (body.user_type == 1) { //employer
-                let a = { name: body.name, email: body.email, phone: "", designation: "", org_name: "", city: "", phone: "" };
+                let a = { name: body.name.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), designation: "", org_name: "", city: "", phone: "" };
                 con.query("INSERT INTO `employerinfo` SET ? ", a, (error1, result1, field1) => {
                     if (error1) {
                         logger.log("error", "Error in adding entry to EMPLOYERINFO table", error1);
@@ -136,7 +137,7 @@ exports.addUser = function (body, res) {
             }
             if (body.user_type == 2) { //employee
                 let a = {
-                    name: body.name, email: body.email, phone: "", looking: 0, fullTime: 1, partTime: 0, intern: 0, designation: "", fut_location: "", experience: 0,
+                    name: body.name.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), phone: "", looking: 0, fullTime: 1, partTime: 0, intern: 0, designation: "", fut_location: "", experience: 0,
                     noticePeriod: 0, skills: "", specificReq: "", gender: 'Male', viewers: 1, objective: "", languages: "", academic_ach: "", extra_curricular: "", certifications: "", hobbies: "", address: "", linkedin: ""
                 };
                 con.query("INSERT INTO `skills` SET ? ", a, (error1, result1, field1) => {
@@ -155,20 +156,20 @@ exports.addUser = function (body, res) {
 }
 
 exports.resendEmail = function (body, res) {
-    let sql = "SELECT v_profile,token,user_type from users where email = '" + body.email + "'";
+    let sql = "SELECT v_profile,token,user_type,name from users where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in resendEmail", error.sqlMessage);
             res.status(500).send(error.sqlMessage);
         } else if (result) {
-            email.sendVerificationMail(body.name, body.email, result[0].token, result[0].v_profile, result[0].user_type);
+            email.sendVerificationMail(result[0].name, body.email, result[0].token, result[0].v_profile, result[0].user_type);
             res.status(200).send("send");
         }
     });
 }
 
 exports.getUserInfo = function (body, res) {
-    let sql = "SELECT email,v_profile,user_type, validity from users where email = '" + body.email + "' AND user_type='" + body.user_type + "' AND token ='" + body.token + "'";
+    let sql = "SELECT email,v_profile,user_type, validity from users where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' AND user_type='" + body.user_type + "' AND token ='" + body.token.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
 
     con.query(sql, (error, result, field) => {
         if (error) {
@@ -181,7 +182,7 @@ exports.getUserInfo = function (body, res) {
 }
 
 exports.changePassword = function (body, res) {
-    let sql = "UPDATE `users` SET password='" + body.new_password + "' where token = '" + body.token + "' AND password = '" + body.old_password + "'";
+    let sql = "UPDATE `users` SET password='" + body.new_password + "' where token = '" + body.token.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' AND password = '" + body.old_password + "'";
 
     con.query(sql, (error, result, field) => {
         if (error) {
--- operations.js
+++ operations.js
@@ -4,7 +4,7 @@ let logger = require('./log/logModule');
 
 /* employerinfo table */
 exports.getEmployerInfo = function (body, res) {
-    let sql = "SELECT * from employerinfo where email = '" + body.email + "'";
+    let sql = "SELECT * from employerinfo where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
 
     con.query(sql, (error, result, field) => {
         if (error) {
@@ -16,7 +16,9 @@ exports.getEmployerInfo = function (body, res) {
     });
 }
 exports.setEmployerInfo = function (body, res) {
-    let sql = "UPDATE `employerinfo` SET name='" + body.name + "',designation='" + body.designation + "',org_name='" + body.org_name + "',city='" + body.city + "',phone='" + body.phone + "' where email = '" + body.email + "'";
+    let sql = "UPDATE `employerinfo` SET name='" + body.name.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "',designation='" + body.designation.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "',org_name='" + body.org_name.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "',city='" + body.city.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "',phone='" + body.phone.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
 
     con.query(sql, (error, result, field) => {
         if (error) {
@@ -32,7 +34,7 @@ exports.setEmployerInfo = function (body, res) {
 exports.getOpenings = function (body, res) {
     let sql = "SELECT * FROM openings ";
     if (body.email == "" && body.ind == "") sql += "ORDER BY timestamp DESC";
-    else if (body.email != "" && body.ind == "") sql += "where email = '" + body.email + "' ORDER BY timestamp DESC";
+    else if (body.email != "" && body.ind == "") sql += "where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' ORDER BY timestamp DESC";
     else if (body.email == "" && body.ind != "") sql += "where ind = '" + body.ind + "' ORDER BY timestamp DESC";
     else { res.status(500).send("Trying to hack huh !!! Hard luck buddy"); }
 
@@ -59,7 +61,11 @@ exports.postRequirements = function (body, res) {
 }
 exports.updateRequirement = function (body, res) {
     let data = JSON.parse(body.obj);
-    let sql = "UPDATE `openings` SET email = '" + data.email + "', fullTime= '" + data.fullTime + "', partTime= '" + data.partTime + "', intern = '" + data.intern + "', designation= '" + data.designation + "', company= '" + data.company + "', location= '" + data.location + "', contact= '" + data.contact + "',	min_years = '" + data.min_years + "',max_years = '" + data.max_years + "', skills= '" + data.skills + "', specificReq= '" + data.specificReq + "', noticePeriod= '" + data.noticePeriod + "', gender= '" + data.gender + "', count= '" + data.count + "' WHERE ind = '" + data.ind + "'";
+    let sql = "UPDATE `openings` SET email = '" + data.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', fullTime= '" + data.fullTime + "', partTime= '" + data.partTime + "', intern = '" + data.intern + 
+    "', designation= '" + data.designation.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', company= '" + data.company.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', location= '" + data.location.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', contact= '" + data.contact.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "',min_years = '" + data.min_years + 
+    "',max_years = '" + data.max_years + "', skills= '" + data.skills.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', specificReq= '" + data.specificReq.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', noticePeriod= '" + data.noticePeriod + "', gender= '" + data.gender + "', count= '" + data.count + "' WHERE ind = '" + data.ind + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in updateRequirement", error);
@@ -94,8 +100,8 @@ exports.getCandidates = function (body, res) {
     });
 }
 exports.getEmployeeInfo = function (body, res) {
-    let sql = "SELECT * from skills where email = '" + body.email + "'";
-    
+    let sql = "SELECT * from skills where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
+
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in getEmployeeInfo", error.sqlMessage);
@@ -113,29 +119,34 @@ exports.getEmployeeInfo = function (body, res) {
 
 exports.setEmployeeInfo = function (body, res) {
     let data = JSON.parse(body.obj);
-    let sql = "UPDATE`skills` SET name='" + data.name + "',phone = '" + data.phone + "', looking='" + data.looking + "', fullTime='" + data.fullTime +
-    "', partTime='" + data.partTime + "', intern='" + data.intern + "', designation='" + data.designation + "', fut_location='" + data.fut_location + "', experience='" + data.experience +
-    "', noticePeriod='" + data.noticePeriod + "', skills='" + data.skills + "', specificReq='" + data.specificReq + "', gender='" + data.gender +
-    "', objective='" + data.objective + "', languages='" + data.languages + "', academic_ach='" + data.academic_ach + "', extra_curricular='" + data.extra_curricular +
-    "', certifications='" + data.certifications + "', hobbies='" + data.hobbies + "', address='" + data.address + "', linkedin='" + data.linkedin + "' where email = '" + body.email + "'";
+    let sql = "UPDATE`skills` SET name='" + data.name.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "',phone = '" + data.phone.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', looking='" + data.looking + "', fullTime='" + data.fullTime + "', partTime='" + data.partTime + "', intern='" + data.intern +
+    "', designation='" + data.designation.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', fut_location='" + data.fut_location.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', experience='" + data.experience + "', noticePeriod='" + data.noticePeriod + "', skills='" + data.skills.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', specificReq='" + data.specificReq.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', gender='" + data.gender +
+    "', objective='" + data.objective.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', languages='" + data.languages.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', academic_ach='" + data.academic_ach.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', extra_curricular='" + data.extra_curricular.replace(/\\/g,'\\\\').replace(/'/g, "\\'") +
+    "', certifications='" + data.certifications.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', hobbies='" + data.hobbies.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "', address='" + data.address.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', linkedin='" + data.linkedin.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+    "' where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
-            if (error) {
-                logger.log("error", "Error in setEmployeeInfo", error);
-                res.status(500).send(error.sqlMessage);
-            } else if (result) {
-                res.status(200).send(result.changedRows.toString());
-            }
-        });
+        if (error) {
+            logger.log("error", "Error in setEmployeeInfo", error);
+            res.status(500).send(error.sqlMessage);
+        } else if (result) {
+            res.status(200).send(result.changedRows.toString());
+        }
+    });
 }
 
 exports.addEducation = function (body, res) {
-    con.query("SELECT MAX(ind) as max FROM `education` WHERE email = '" + body.email + "'", (error, result, field) => {
+    con.query("SELECT MAX(ind) as max FROM `education` WHERE email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'", (error, result, field) => {
         if (error) {
             logger.log("error", "Error in adding education table count", error);
             res.status(500).end(error.sqlMessage);
         } else if (result) {
             let max = result[0].max + 1
-            let data = { email: body.email, ind: max, college: '', degree: '', stream: '', start: '', end: '', cgpa: 0, percentage: 0 };
+            let data = { email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), ind: max, college: '', degree: '', stream: '', start: '', end: '', cgpa: 0, percentage: 0 };
             con.query("INSERT INTO `education` SET ? ", data, (error1, result1, field) => {
                 if (error1) {
                     logger.log("error", "Error in inserting education table", error1.sqlMessage);
@@ -149,7 +160,7 @@ exports.addEducation = function (body, res) {
     });
 }
 exports.getEducation = function (body, res) {
-    let sql = "SELECT * from education where email = '" + body.email + "' ORDER BY end DESC";
+    let sql = "SELECT * from education where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' ORDER BY end DESC";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in get Education table data", error.sqlMessage);
@@ -162,8 +173,10 @@ exports.getEducation = function (body, res) {
 
 exports.updateEducation = function (body, res) {
     let sql = "UPDATE `education` SET " +
-        "college='" + body.college + "', degree ='" + body.degree + "', stream ='" + body.stream + "', start ='" + body.start + "', end ='" + body.end + "', cgpa ='" + body.cgpa + "', percentage ='" + body.percentage +
-        "' WHERE ind ='" + body.ind + "' AND email='" + body.email + "'";
+        "college='" + body.college.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', degree ='" + body.degree.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+        "', stream ='" + body.stream.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', start ='" + body.start +
+         "', end ='" + body.end + "', cgpa ='" + body.cgpa + "', percentage ='" + body.percentage +
+        "' WHERE ind ='" + body.ind + "' AND email='" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in updateEducation:", error.sqlMessage);
@@ -179,7 +192,7 @@ exports.updateEducation = function (body, res) {
 }
 
 exports.deleteEducation = function (body, res) {
-    con.query("DELETE FROM `education` WHERE ind ='" + body.ind + "' AND email='" + body.email + "'", (error, result, field) => {
+    con.query("DELETE FROM `education` WHERE ind ='" + body.ind + "' AND email='" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'", (error, result, field) => {
         if (error) {
             logger.log("error", "Error in deleteEducation:", error.sqlMessage);
             res.status(500).end(error.sqlMessage);
@@ -191,13 +204,13 @@ exports.deleteEducation = function (body, res) {
 }
 
 exports.addWork = function (body, res) {
-    con.query("SELECT MAX(ind) as max FROM `work_experience` WHERE email = '" + body.email + "'", (error, result, field) => {
+    con.query("SELECT MAX(ind) as max FROM `work_experience` WHERE email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'", (error, result, field) => {
         if (error) {
             logger.log("error", "Error in adding work_experience table count", error);
             res.status(500).end(error.sqlMessage);
         } else if (result) {
             let max = result[0].max + 1
-            let data = { email: body.email, ind: max, company: '', description: '', start: '', end: '',present: false };
+            let data = { email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), ind: max, company: '', description: '', start: '', end: '',present: false };
             con.query("INSERT INTO `work_experience` SET ? ", data, (error1, result1, field) => {
                 if (error1) {
                     logger.log("error", "Error in inserting work_experience table", error1.sqlMessage);
@@ -212,7 +225,7 @@ exports.addWork = function (body, res) {
 }
 
 exports.getWork = function (body, res) {
-    let sql = "SELECT * from work_experience where email = '" + body.email + "' ORDER BY end DESC";
+    let sql = "SELECT * from work_experience where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' ORDER BY end DESC";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in get work_experience table data", error.sqlMessage);
@@ -224,8 +237,10 @@ exports.getWork = function (body, res) {
 }
 exports.updateWork = function (body, res) {
     let sql = "UPDATE `work_experience` SET " +
-        "company='" + body.company + "', description ='" + body.description + "', start ='" + body.start + "', end ='" + body.end + "',present =" + body.present + " WHERE ind ='" + body.ind + "' AND email='" + body.email + "'";
-    
+        "company='" + body.company.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', description ='" + body.description.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+        "', start ='" + body.start + "', end ='" + body.end + "',present =" + body.present + 
+        " WHERE ind ='" + body.ind + "' AND email='" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
+    console.log("sql :", sql);
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in update work_experience:", error.sqlMessage);
@@ -241,7 +256,7 @@ exports.updateWork = function (body, res) {
 }
 
 exports.deleteWork = function (body, res) {
-    con.query("DELETE FROM `work_experience` WHERE ind ='" + body.ind + "' AND email='" + body.email + "'", (error, result, field) => {
+    con.query("DELETE FROM `work_experience` WHERE ind ='" + body.ind + "' AND email='" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'", (error, result, field) => {
         if (error) {
             logger.log("error", "Error in delete work_experience:", error.sqlMessage);
             res.status(500).end(error.sqlMessage);
@@ -253,13 +268,13 @@ exports.deleteWork = function (body, res) {
 }
 
 exports.addProject = function (body, res) {
-    con.query("SELECT MAX(ind) as max FROM `projects` WHERE email = '" + body.email + "'", (error, result, field) => {
+    con.query("SELECT MAX(ind) as max FROM `projects` WHERE email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'", (error, result, field) => {
         if (error) {
             logger.log("error", "Error in adding projects table count", error);
             res.status(500).end(error.sqlMessage);
         } else if (result) {
             let max = result[0].max + 1
-            let data = { email: body.email, ind: max, name: '', description: '', skills: '', start: '', end: '' };
+            let data = { email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), ind: max, name: '', description: '', skills: '', start: '', end: '' };
             con.query("INSERT INTO `projects` SET ? ", data, (error1, result1, field) => {
                 if (error1) {
                     logger.log("error", "Error in inserting projects table", error1.sqlMessage);
@@ -274,7 +289,7 @@ exports.addProject = function (body, res) {
 }
 
 exports.getProject = function (body, res) {
-    let sql = "SELECT * from projects where email = '" + body.email + "' ORDER BY end DESC";
+    let sql = "SELECT * from projects where email = '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "' ORDER BY end DESC";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in get projects table data", error.sqlMessage);
@@ -286,7 +301,9 @@ exports.getProject = function (body, res) {
 }
 exports.updateProject = function (body, res) {
     let sql = "UPDATE `projects` SET " +
-        "name='" + body.name + "', description ='" + body.description + "', skills ='" + body.skills + "', start ='" + body.start + "', end ='" + body.end + "' WHERE ind ='" + body.ind + "' AND email='" + body.email + "'";
+        "name='" + body.name.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', description ='" + body.description.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + 
+        "', skills ='" + body.skills.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "', start ='" + body.start + "', end ='" + body.end + 
+        "' WHERE ind ='" + body.ind + "' AND email='" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     con.query(sql, (error, result, field) => {
         if (error) {
             logger.log("error", "Error in update projects:", error.sqlMessage);
@@ -302,7 +319,7 @@ exports.updateProject = function (body, res) {
 }
 
 exports.deleteProject = function (body, res) {
-    con.query("DELETE FROM `projects` WHERE ind ='" + body.ind + "' AND email='" + body.email + "'", (error, result, field) => {
+    con.query("DELETE FROM `projects` WHERE ind ='" + body.ind + "' AND email='" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'", (error, result, field) => {
         if (error) {
             logger.log("error", "Error in delete projects:", error.sqlMessage);
             res.status(500).end(error.sqlMessage);
@@ -314,7 +331,7 @@ exports.deleteProject = function (body, res) {
 }
 /* feedback table */
 exports.submitFeedback = function (body, res) {
-    let data = { email: body.email, profile: body.profile, subject: body.subject };
+    let data = { email: body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'"), profile: body.profile, subject: body.subject.replace(/\\/g,'\\\\').replace(/'/g, "\\'") };
     // console.log("data",data);
     con.query("INSERT INTO `feedback` SET ? ", data, (error, result, field) => {
         if (error) {
@@ -334,7 +351,7 @@ exports.getSuperTables = function (body, res) {
     if (body.email == "") {
         sql = "SELECT * FROM " + body.tableName;
     } else {
-        sql = "SELECT * FROM " + body.tableName + " WHERE email= '" + body.email + "'";
+        sql = "SELECT * FROM " + body.tableName + " WHERE email= '" + body.email.replace(/\\/g,'\\\\').replace(/'/g, "\\'") + "'";
     }
 
     con.query(sql, (error, result, field) => {
